5492f2e9e08a0f7331ed6fd05ae294728d7b881f
Sultanova Karina <karinarafaelevna@gmail.com>
2024-11-03 15:17:12 +0300

    deploy 2

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+const BASE_PATH = process.env.PUBLIC_URL || '';
+
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-     <Router basename="/monitoring">
+    <Router basename={BASE_PATH}>
       <App />
     </Router>
   </React.StrictMode>
